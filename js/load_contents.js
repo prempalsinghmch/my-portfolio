1385e858b86b76a0c24ec134908a8ab609b45b99
korku02 <korkudeepak@gmail.com>
2020-12-20 14:44:00 +0530

    minor changes

--- js/load_contents.js
+++ js/load_contents.js
@@ -4,16 +4,7 @@
         
         $(".navbar").load("nav.html"); 
 
-        $(document).ready(function(){
-          let deps=get_deps();
-          console.log(deps);
-          let i=0;
-          for (i; i<deps.length; i++){
-            let x = deps[i]
-            let str = `<a class="dropdown-item" href="/treatments.html?name=${x}">${x}</a>`
-            $('.dropdown-menu').append(str);
-          }
-        });
+       
 
       });
       $(function(){
